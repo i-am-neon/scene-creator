dc2516e22958f38c450bc469e3629f4c0b246fb1
NΞ◎N <90662438+i-am-neon@users.noreply.github.com>
2024-12-25 19:54:48 -0600

    add shadcn ui

--- app/test-supabase/page.tsx
+++ app/test-supabase/page.tsx
@@ -1,12 +1,15 @@
 "use client";
+import { Button } from "@/components/ui/button";
 import { addStory, getStory } from "./actions";
 
 export default function TestSupabasePage() {
   return (
     <div>
       <h1>Test Supabase Page</h1>
-      <button onClick={addStory}>Add Story</button>
-      <button onClick={getStory}>Read Story</button>
+      <Button onClick={addStory}>Add Story</Button>
+      <br />
+      <br />
+      <Button onClick={getStory}>Read Story</Button>
     </div>
   );
 }
